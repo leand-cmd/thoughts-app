73480bc7185689b9f479347b504f1bf7c6bd07c2
leand-cmd <caceresamarillaleandro@gmail.com>
2026-08-04 00:10:28 -0300

    Force PostgreSQL and add migration release task

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
+release: python manage.py migrate --noinput
 web: gunicorn config.wsgi
-release: python manage.py migrate
--- config/settings.py
+++ config/settings.py
@@ -82,12 +82,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
